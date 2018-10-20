1b1ffe4f3652217a98bca63c3b3ee86040e2127c
Gregor von Laszewski <laszewski@gmail.com>
2018-10-20 05:03:36 -0400

    add bibtex check

--- bin/check-bibtex.py
+++ bin/check-bibtex.py
@@ -3,23 +3,30 @@ import sys
 from textwrap import indent
 
 biblog = sys.argv[1]
-bibfile = sys.argv[2]
+changedfiles = sys.argv[2]
+
+with open(changedfiles, "r") as f:
+    files = f.read().split()
 
 with open(biblog, "r") as f:
     log = f.read()
 
-with open(bibfile, "r") as f:
-    content = f.read()
-
 cites = log.replace("pandoc-citeproc: reference ", "").replace(" not found","").split()
 
-error_list = []
-error = False
-for cite in cites:
-    if cite + "]" in content:
-        error = True
-        error_list.append(cite)
-if error:
-    print ("ERROR: Bibtex entries not defined:")
-    print(indent('\n'.join(error_list), '       * ', lambda line: True))
-    sys.exit(1)
+for bibfile in files:    
+
+    print ("B", bibfile)
+    with open(bibfile, "r") as f:
+        content = f.read()
+
+
+    error_list = []
+    error = False
+    for cite in cites:
+        if cite + "]" in content:
+            error = True
+            error_list.append(cite)
+    if error:
+        print ("ERROR: Bibtex entries not defined:")
+        print(indent('\n'.join(error_list), '       * ', lambda line: True))
+        sys.exit(1)
